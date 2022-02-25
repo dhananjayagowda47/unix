f3b9e300284291aece8f051ee75ea12d25c7b276
dhananjayagowda47 <100307296+dhananjayagowda47@users.noreply.github.com>
2022-02-25 21:49:28 +0530

    Add files via upload

---
+++ 7a.sh
@@ -0,0 +1,25 @@
+set -- `who`
+u=`whoami`
+n=5
+for i in $*
+do
+if [ $u = $i ]
+then
+t=$4
+
+fi
+done
+set -- `echo $t | tr ":" " "`
+h=$1
+if [ $h -ge 4 ] && [ $h -lt 12 ]
+then
+echo "Good morning Mr./Mrs. $u"
+elif [ $h -ge 12 ] && [ $h -lt 16 ]
+then
+echo "Good Afternoon Mr./Mrs. $u"
+elif [ $h -ge 16 ] && [ $h -lt 19 ]
+then
+echo "Good Evening Mr./Mrs. $u"
+else
+echo "Good Night Mr./Mrs. $u"
+fi
